bbfc433372a0e06d9e12b48603dd3281aad9417d
yazan Abualoun <1210145@student.birzeit.edu>
2025-05-08 00:50:11 +0300

    BUILD: Add Room Database dependency
    Adds the Room persistence library dependencies to the project. This includes the core runtime, compiler, Kotlin extensions, RxJava 2 and 3 support, Guava support, testing helpers, and Paging 3 integration. Also updates the `.gitignore` file to ignore the `.idea` directory.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,28 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(libs.androidx.room.runtime)
+
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    annotationProcessor(libs.room.compiler)
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation(libs.androidx.room.ktx)
+
+    // optional - RxJava2 support for Room
+    implementation(libs.androidx.room.rxjava2)
+
+    // optional - RxJava3 support for Room
+    implementation(libs.androidx.room.rxjava3)
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation(libs.androidx.room.guava)
+
+    // optional - Test helpers
+    testImplementation(libs.androidx.room.testing)
+
+    // optional - Paging 3 Integration
+    implementation(libs.androidx.room.paging)
 }
